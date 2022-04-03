b80facfe894ffa3f081061fc028979a5842aac95
Gadji <darkkukri0550@gmail.com>
2022-04-03 14:15:27 +0300

    Пробелы

--- app/Http/Resources/ToursResource.php
+++ app/Http/Resources/ToursResource.php
@@ -15,33 +15,27 @@ class ToursResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
 
-            'id'=> $this->id,
+            'route_number' => $this->route_number,
+            'route_depart_station' => $this->route_depart_station,
+            'route_arrival_station'=> $this->route_arrival_station,
+            'route_departure_address' => $this->route_departure_address,
+            'route_arrival_address' => $this->route_arrival_address,
 
-            'route_number '=> $this->route_number,
-            'route_depart_station '=> $this->route_depart_station,
-            'route_arrival_station '=> $this->route_arrival_station,
-            'route_departure_address '=> $this->route_departure_address,
-            'route_arrival_address '=> $this->route_arrival_address,
+            'vehicle' => $this->vehicle,
+            'vehicle_type ' => $this->vehicle_type,
+            'vehicle_model_vehicle' => $this->vehicle_model_vehicle,
+            'vehicle_gus_number_vehicle' => $this->vehicle_gus_number_vehicle,
 
-            'vehicle '=> $this->vehicle,
-            'vehicle_type '=> $this->vehicle_type,
-            'vehicle_model_vehicle	 '=> $this->vehicle_model_vehicle,
-            'vehicle_gus_number_vehicle	 '=> $this->vehicle_gus_number_vehicle,
+            'driver' => $this->driver,
+            'driver_2' => $this->driver_2,
 
-
-            'driver	'=> $this->driver,
-            'driver_2 '=> $this->driver_2,
-
-            'depart_time'=>$this->depart_time,
-            'arrival_time'=>$this->arrival_time,
-            'departure_date'=>$this->departure_date,
-            'arrival_date'=>$this->arrival_date,
-            'condition_tours'=>$this->condition_tours,
-
-
-
-
+            'depart_time' => $this->depart_time,
+            'arrival_time' => $this->arrival_time,
+            'departure_date' => $this->departure_date,
+            'arrival_date' => $this->arrival_date,
+            'condition_tours' => $this->condition_tours,
         ];
     }
 }
